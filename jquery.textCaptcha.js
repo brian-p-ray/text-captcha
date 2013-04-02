95dae11b46e387af557b298072319d84ce4b1b85
Brian Ray <brian.ray@area203.com>
2013-04-02 08:52:47 -0400

    Cleaned up code, swapped out == with ===, != with !==

--- jquery.textCaptcha.js
+++ jquery.textCaptcha.js
@@ -7,7 +7,7 @@
 */
 (function($){
 
-	var ver = '0.1';
+	var ver = '0.3';
 
 	// provide log functionality for devs
 	function log() {
@@ -15,17 +15,17 @@
 	}
 
 	$.fn.textCaptcha = function(options) {
-    
+
 		var defaults = {
 			type: 'text', // Type of characters to display for captcha. Can be text, number or random.
 			method: 'alphabetic', // Type of sort to require the user to perform. Can be alphabetic, reverse_alphabetic, backwards or random if  type=text. Can be add, multiply or random if type=math
 			length: 4 // Number of characters to display. I would suggest 4 to 6. Only applies if type=text.
 		};
-  
+
 		var options = $.extend(defaults, options);
 
 		return this.each(function() {
-			
+
 			var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
 				numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 				operators = {'add': '+', 'multiply': '*'},
@@ -34,40 +34,39 @@
 				math_methods = ['add', 'multiply'],
 				number_methods = ['numeric', 'reverse_numeric'],
 				captcha_operator = '',
-				captcha_string = '', num1, num2, operator, // set strings to hold the random values
+				captcha_string = '', num1, num2, // set strings to hold the random values
 				captcha_method_text;
-			
+
 			// choose type if options.type == random
-			if (options.type == 'random') {
+			if (options.type === 'random') {
 				set_random_type();
 			}
-			
+
 			// verify that the method matches the type
 			var methods;
 			switch(options.type) {
 				case 'text':
 					methods = text_methods;
 				break;
-				
+
 				case 'math':
 					methods = math_methods;
-				break
-				
+				break;
+
 				case 'number':
 					methods = number_methods;
 				break;
 			}
-			
+
 			// choose method if options.method == random
-			if (options.method == 'random') {
+			if (options.method === 'random') {
 				set_random_method();
 			}
-			
+
 			set_captcha();
-			
+
 			mismatch_error();
 
-			
 			//display text based on captcha_method chosen
 			switch(options.method) {
 				// text
@@ -78,71 +77,70 @@
 				case 'alphabetic':
 					captcha_method_text = 'Type this string in alphabetical order';
 				break;
-				
+
 				case 'reverse_alphabetic':
 					captcha_method_text = 'Type this string in reverse alphabetical order';
 				break;
-				
 				// math
 				case 'add':
 					captcha_method_text = 'Add these numbers';
 				break;
-				
+
 				case 'multiply':
 					captcha_method_text = 'Multiply these numbers';
 				break;
-				
+
 				// number
 				case 'numeric':
 					captcha_method_text = 'Put these numbers in numeric order';
 				break;
-				
+
 				case 'reverse_numeric':
 					captcha_method_text = 'Put these numbers in reverse numeric order';
 				break;
 			}
-			
+
 			output($(this));
-			
+
 			// set type if random
 			function set_random_type() {
 				options.type = types[Math.floor(Math.random()*types.length)];
 				options.method = 'random';
 			}
-			
+
 			// set method if random
 			function set_random_method() {
 				switch(options.type) {
 					case 'text':
 						options.method = text_methods[Math.floor(Math.random()*text_methods.length)];
 					break;
-					
+
 					case 'math':
 						options.method = math_methods[Math.floor(Math.random()*math_methods.length)];
 					break;
-					
+
 					case 'number':
 						options.method = number_methods[Math.floor(Math.random()*number_methods.length)];
 					break;
 				}
 			}
-			
+
 			function set_captcha() {
 				switch (options.type) {
 					case 'text':
-						for (i=0; i<options.length; i++) {
+						for (var i=0; i<options.length; i++) {
 							captcha_string += characters[Math.floor(Math.random()*characters.length)];
 						}
 					break;
-					
+
 					case 'math':
 						captcha_operator = operators[options.method];
 						num1 = numbers[Math.floor(Math.random()*numbers.length)];
 						num2 = numbers[Math.floor(Math.random()*numbers.length)];
-						
+
 						captcha_string = num1 + ' ' + captcha_operator + ' ' + num2;
 					break;
-					
+
 					case 'number':
 						for (i=0; i<options.length; i++) {
 							captcha_string += numbers[Math.floor(Math.random()*numbers.length)];
@@ -150,10 +148,10 @@
 					break;
 				}
 			}
-			
+
 			function output(el) {
 				var identifier = '';
-				for (i=0; i<numbers.length; i++) {
+				for (var i=0; i<numbers.length; i++) {
 					identifier += numbers[Math.floor(Math.random()*numbers.length)];
 				}
 				// find element where id starts with...
@@ -163,20 +161,20 @@
 				output += '<input type="hidden" id="captcha_type_'+ identifier + '" name="captcha_type" value="'+options.type+'" />';
 				output += '<input type="hidden" id="captcha_method_'+ identifier + '" name="captcha_method" value="'+options.method+'" />';
 				output += '<input type="hidden" id="captcha_value_'+ identifier + '" name="captcha_value" value="'+captcha_string+'" />';
-	
+
 				el.find(':submit').before(output);
 			}
-			
+
 			function mismatch_error() {
 				if ($.inArray(options.method, methods) < 0) {
 					log('Type, method mismatch. \n type: '+options.type+' \n method: '+options.method);
 					return false;
 				}
 			}
-			
+
 		});
 	};
-	
+
 	$.fn.textCaptchaCheck = function() {
 		var $this = $(this);
 		var value,
@@ -184,8 +182,8 @@
 			captcha_type = $this.find('input[name="captcha_type"]').val(),
 			captcha_method = $this.find('input[name="captcha_method"]').val(),
 			captcha_user = $this.find('input[name="captcha_user"]').val();
-		
-		if (captcha_type == 'text' || captcha_type == 'number') {
+
+		if (captcha_type === 'text' || captcha_type === 'number') {
 			value = $this.find('input[name="captcha_value"]').val().split(""); //grab the captcha value and turn it into an array
 		}
 		else {
@@ -197,49 +195,49 @@
 			case 'alphabetic':
 				value = value.sort(); //if alphabetic, sort the array
 			break;
-			
+
 			case 'backwards': //if backwards, reverse the array
 				value = value.reverse();
 			break;
-			
+
 			case 'reverse_alphabetic': // if reverse alphabetic, sort the array and reverse
 				value = value.sort();
 				value = value.reverse();
 			break;
-			
+
 			// math
 			case 'add':
 				value = eval(value);
 			break;
-			
+
 			case 'multiply':
 				value = eval(value);
 			break;
-			
+
 			//number
 			case 'numeric':
-				value = value.sort(function(a,b){return a - b});
+				value = value.sort(function(a,b){return a - b;});
 			break;
-			
+
 			case 'reverse_numeric':
-				value = value.sort(function(a,b){return a - b});
+				value = value.sort(function(a,b){return a - b;});
 				value = value.reverse();
 			break;
 		}
-		
-		if (captcha_type == 'text' || captcha_type == 'number') {
+
+		if (captcha_type === 'text' || captcha_type === 'number') {
 			correct_value = value.join(''); //turn the array back into a string
 		}
 		else {
 			correct_value = value;
 		}
-		
-		if (captcha_user != correct_value) { //if the user inputted value does not match the modified string
+
+		if (captcha_user !== correct_value) { //if the user inputted value does not match the modified string
 			return false;
 		}
 		else {
 			return true;
 		}
 	};
-	
+
 })(jQuery);
